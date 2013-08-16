786d8853e54852cdc516671555769a7073c07f9d
Joshua Moody <joshuajmoody@gmail.com>
2013-08-17 01:59:13 +0200

    record_end saves to PLAYBACK_DIR or ./features/playback and the default playback search directory is changed from ./ to ./features/playback

--- calabash-cucumber/lib/calabash-cucumber/core.rb
+++ calabash-cucumber/lib/calabash-cucumber/core.rb
@@ -407,7 +407,7 @@ EOF
         end
 
         # this should probably default to ./features/playback and not to ./playback
-        rec_dir = ENV['PLAYBACK_DIR'] || "#{Dir.pwd}/playback"
+        rec_dir = ENV['PLAYBACK_DIR'] || "#{Dir.pwd}/features/playback"
 
         recording = recording_name_for(recording_name, os, device)
         data = load_recording(recording, rec_dir)
@@ -524,15 +524,16 @@ EOF
         system("openssl base64 -in _recording_binary.plist -out '#{file_name}'")
         system("rm _recording.plist _recording_binary.plist")
 
-        if ENV['PLAYBACK_DIR']
-          dir = ENV['PLAYBACK_DIR']
-          unless Dir.exists?(dir)
-            system("mkdir -p #{dir}")
+        rec_dir = ENV['PLAYBACK_DIR'] || "#{Dir.pwd}/features/playback"
+        unless Dir.exists?(rec_dir)
+          if ENV['CALABASH_FULL_CONSOLE_OUTPUT'] == '1'
+            puts "creating playback directory at '#{rec_dir}'"
           end
-          system("mv #{file_name} #{dir}")
+          system("mkdir -p #{rec_dir}")
         end
 
-        file_name
+        system("mv #{file_name} #{rec_dir}")
+        "#{file_name} ==> '#{rec_dir}/#{file_name}'"
       end
 
       def backdoor(sel, arg)
