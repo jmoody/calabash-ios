5953265a79adbe140fe81beed84f54abd73aaefa
Joshua Moody <joshuajmoody@gmail.com>
2014-07-17 14:53:12 +0200

    WIP file paths....

--- script/add-keychain.sh
+++ script/add-keychain.sh
@@ -6,4 +6,4 @@ security -v set-keychain-settings -lut 86400 ios-build.keychain
 security import ./script/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./script/dev.cer   -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./script/dev.p12   -k ~/Library/Keychains/ios-build.keychain -P $DEV_P12_PASSWORD
-open "script/${TRAVIS_PROVISIONING_PROFILE}"
+open "${TRAVIS_PROVISIONING_PROFILE}"
