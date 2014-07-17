3c358ae464acfc94da46adf1b8cb866228082675
Joshua Moody <joshuajmoody@gmail.com>
2014-07-17 12:02:35 +0200

    WIP file paths in add-keychain.sh

--- script/add-keychain.sh
+++ script/add-keychain.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 security create-keychain -p travis ios-build.keychain
-security import ./script/travis/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./script/travis/dev.cer   -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./script/travis/dev.p12   -k ~/Library/Keychains/ios-build.keychain -P $DEV_P12_PASSWORD
+security import ./script/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./script/dev.cer   -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./script/dev.p12   -k ~/Library/Keychains/ios-build.keychain -P $DEV_P12_PASSWORD
