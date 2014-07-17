fe9f1fc948462e5c77776e631c80231b5dda535e
Joshua Moody <joshuajmoody@gmail.com>
2014-07-17 12:58:50 +0200

    WIP need mobileprovision

--- script/add-keychain.sh
+++ script/add-keychain.sh
@@ -6,3 +6,5 @@ security -v set-keychain-settings -lut 86400 ios-build.keychain
 security import ./script/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./script/dev.cer   -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./script/dev.p12   -k ~/Library/Keychains/ios-build.keychain -P $DEV_P12_PASSWORD
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+cp ./script/travis.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
--- script/remove-keychain.sh
+++ script/remove-keychain.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 security delete-keychain ios-build.keychain
+rm -rf ~/Library/MobileDevice/Provisioning\ Profiles/*.mobileprovision
