ae43b1a686eb3c6fa16bb7528eefca84d1cb91ac
Joshua Moody <joshuajmoody@gmail.com>
2014-07-17 15:13:36 +0200

    WIP more madnesss

--- script/add-keychain.sh
+++ script/add-keychain.sh
@@ -6,4 +6,6 @@ security -v set-keychain-settings -lut 86400 ios-build.keychain
 security import ./script/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./script/dev.cer   -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./script/dev.p12   -k ~/Library/Keychains/ios-build.keychain -P $DEV_P12_PASSWORD
-open "${TRAVIS_PROVISIONING_PROFILE}"
+PROFILE_DIR="${PWD}/Library/MobileDevice/Provisioning Profiles"
+mkdir -p "${PROFILE_DIR}"
+cp "${TRAVIS_PROVISIONING_PROFILE}" "${PROFILE_DIR}"
